b7a6104424dc151a4a56bde9b6cd462708ecd5c8
Raphael Discky <zundria.putra@gmail.com>
2020-09-20 21:11:46 +0700

    Create custom auth middleware and JWT verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const authorize = require('../../middleware/authorize');
+
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public (doesn't need a token)
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', authorize, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -56,13 +58,25 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken (if user get token, then user can login right away)
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '1h' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
     }
-
-    res.send('Users route');
   }
 );
 
