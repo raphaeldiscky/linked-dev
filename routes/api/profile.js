154cb9fb664bb8db96ce76d93b056a825dc40c53
Raphael Discky <zundria.putra@gmail.com>
2020-09-21 21:06:53 +0700

    Delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -158,7 +158,7 @@ router.put(
     authorize,
     [
       check('title', 'Title is required').not().isEmpty(),
-      check('companfiny', 'Company is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
       check('from', 'From date is required').not().isEmpty()
     ]
   ],
@@ -192,11 +192,28 @@ router.put(
       profile.experience.unshift(newExp);
       await profile.save();
       res.json(profile);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
-    }
+    } catch (err) {}
   }
 );
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete profile experience
+// @access  Private
+router.delete('/experience/:exp_id', authorize, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
