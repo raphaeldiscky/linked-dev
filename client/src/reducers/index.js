c6be6c333828dbc7a50bdc4b4308ee98348447f4
Raphael Discky <zundria.putra@gmail.com>
2020-09-23 23:57:36 +0700

    Create alert reducer, action, and types

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,7 @@
-// we're gonna have multiple reducer
+// we're gonna have multiple reducer so using combineReducers
 import { combineReducers } from 'redux';
+import alert from './alert';
 
-export default combineReducers({});
+export default combineReducers({
+  alert
+});
