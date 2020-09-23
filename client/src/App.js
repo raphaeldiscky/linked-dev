bb575de94d943682f7ec72c07e63eb97e3f0612a
Raphael Discky <zundria.putra@gmail.com>
2020-09-23 21:25:33 +0700

    Set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React, { Fragment } from 'react';
-import Navbar from './components/Layout/Navbar';
-import Landing from './components/Layout/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        {/*Route Landing => put outside container, because we want the image to go all the way over*/}
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            {/*Use switch for private component*/}
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 };
 
