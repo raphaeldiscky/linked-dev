39709e6c5618ac06b1081b1e5743cb78cce23ccc
Raphael Discky <zundria.putra@gmail.com>
2020-09-23 22:08:49 +0700

    Build login form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 // redux => to make a request to the backend
 
 const Register = () => {
@@ -20,24 +20,7 @@ const Register = () => {
     if (password !== password2) {
       console.log('Passwords do not match');
     } else {
-      const newUser = {
-        name,
-        email,
-        password
-      };
-      try {
-        const config = {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        };
-        const body = JSON.stringify(newUser);
-        const res = await axios.post('/api/users', body, config); // post is from users route hitting @route POST /api/users
-        // we using proxy so we can use /api/users instead of http://localhost:5000
-        console.log(res.data);
-      } catch (err) {
-        console.error(err.response.data);
-      }
+      console.log('Success');
     }
   };
   return (
@@ -93,7 +76,7 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
