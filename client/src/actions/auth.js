357bc4584751f455cac7e03905cf6ae3e7cc3c64
Raphael Discky <zundria.putra@gmail.com>
2020-09-25 21:04:37 +0700

    Create welcome on the  dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -82,7 +83,8 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
-// Logout User/ Clear Profile
+// Logout User / Clear Profile
 export const logout = () => (dispatch) => {
   dispatch({ type: LOGOUT });
+  dispatch({ type: CLEAR_PROFILE });
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -20,3 +20,6 @@ export const LOGOUT = 'LOGOUT';
 // Profile
 export const GET_PROFILE = 'GET_PROFILE';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
+
+// Clear Profile when logout
+export const CLEAR_PROFILE = 'CLEAR_PROFILE';
