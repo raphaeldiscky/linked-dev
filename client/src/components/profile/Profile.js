ae499b6e2ddd32c70f863da775b44fe1b9806768
Raphael Discky <zundria.putra@gmail.com>
2020-09-27 15:59:20 +0700

    Display github repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({
   getProfileById,
@@ -67,6 +68,9 @@ const Profile = ({
                 <h4>No experience credential</h4>
               )}
             </div>
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
