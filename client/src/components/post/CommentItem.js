1d8cde312946136001b1edef5dbd8c1f554e019b
Raphael Discky <zundria.putra@gmail.com>
2020-09-27 23:36:35 +0700

    Display comment and Delete comment

---
+++ client/src/components/post/CommentItem.js
@@ -0,0 +1,52 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import { deleteComment } from '../../actions/post';
+
+const CommentItem = ({
+  postId,
+  deleteComment,
+  comment: { _id, text, name, avatar, user, date },
+  auth
+}) => {
+  return (
+    <div class='post bg-white p-1 my-1'>
+      <div>
+        <Link ro={`/profile/${user}`}>
+          <img class='round-img' src={avatar} alt='' />
+          <h4>{name}</h4>
+        </Link>
+      </div>
+      <div>
+        <p class='my-1'>{text}</p>
+        <p class='post-date'>
+          Posted on <Moment format='DD/MM/YYYY'>{date}</Moment>
+        </p>
+        {!auth.loading && user === auth.user._id && (
+          <button
+            type='button'
+            className='btn btn-danger'
+            onClick={(e) => deleteComment(postId, _id)}
+          >
+            <i className='fas fa-trash' />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+CommentItem.propTypes = {
+  postId: PropTypes.number.isRequired,
+  deleteComment: PropTypes.func.isRequired,
+  comment: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { deleteComment })(CommentItem);
