33fb7cd9ab1f716705cec9f0564375ba362928bc
Raphael Discky <zundria.putra@gmail.com>
2020-09-27 19:09:47 +0700

    Create PostItem component

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+
+const PostItem = ({
+  auth,
+  post: { _id, text, name, avatar, user, upvotes, comments, date }
+}) => {
+  return (
+    <div class='post bg-white p-1 my-1'>
+      <div>
+        <a href='profile.html'>
+          <img class='round-img' src={avatar} alt='' />
+          <h4>{name}</h4>
+        </a>
+      </div>
+      <div>
+        <p class='my-1'>{text}</p>
+        <p class='post-date'>
+          Posted on <Moment format='DD/MM/YYYY'>{date}</Moment>
+        </p>
+        <button type='button' class='btn btn-light'>
+          <i class='fas fa-angle-up'></i>{' '}
+          <span>{upvotes.length > 0 && <span>{upvotes.length}</span>}</span>
+        </button>
+        <button type='button' class='btn btn-light'>
+          <i class='fas fa-angle-down'></i>
+        </button>
+        <Link to={`/post/${_id}`} class='btn btn-primary'>
+          Discussion{' '}
+          {comments.length > 0 && (
+            <span class='comment-count'> {comments.length}</span>
+          )}
+        </Link>
+        {!auth.loading && user === auth.user._id && (
+          <button type='button' class='btn btn-danger'>
+            <i class='fas fa-times' />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+PostItem.propTypes = {
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, {})(PostItem);
